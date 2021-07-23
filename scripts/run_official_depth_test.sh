11f255b4a6adc2378681ffd1c508662087422756
QiukuZ <zhangqk98@zju.edu.cn>
2021-07-23 15:36:16 +0800

    feat(scripts): add scripts

---
+++ scripts/run_official_depth_test.sh
@@ -0,0 +1,8 @@
+python ../test.py \
+--data ../dataset/test/ \
+--sequence-length 2 \
+--pretrained-dps ../model/depth_checkpoint.pth.tar \
+--pose_init demon \
+--save test0 \
+--output-print \
+--output-dir ../result_tmp
